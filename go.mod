df77fb0b30d2bfacf1d77867f79c115ec3936542
jiayu_guo <jiayu_guo@paradise-soft.com.tw>
2020-03-27 13:44:06 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module restful_CRUD
+
+go 1.12
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/mux v1.7.4
+)
---
+++ main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Age  string `json:"age"`
+}
+
+var db *sql.DB
+var err error
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var users []User
+
+	result, err := db.Query("SELECT * FROM user")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+		var user User
+		err := result.Scan(&user.ID, &user.Name, &user.Age)
+		if err != nil {
+			panic(err.Error())
+		}
+		users = append(users, user)
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	stmt, err := db.Prepare("INSERT INTO user (name, age) VALUES (?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	name := keyVal["name"]
+	age := keyVal["age"]
+
+	_, err = stmt.Exec(name, age)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "New User was created.")
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	result, err := db.Query("SELECT * FROM user WHERE id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	var user User
+	for result.Next() {
+		err := result.Scan(&user.ID, &user.Name, &user.Age)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	stmt, err := db.Prepare("UPDATE user SET name  = ?, age = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newName, existsName := keyVal["name"]
+	newAge, existsAge := keyVal["age"]
+
+	if !existsName {
+		stmt, err = db.Prepare("UPDATE user SET age = ? WHERE id = ?")
+		_, err = stmt.Exec(newAge, params["id"])
+		if err != nil {
+			panic(err.Error())
+		}
+	} else if !existsAge {
+		stmt, err = db.Prepare("UPDATE user SET name = ? WHERE id = ?")
+
+		_, err = stmt.Exec(newName, params["id"])
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		_, err = stmt.Exec(newName, newAge, params["id"])
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	fmt.Fprintf(w, "User with ID = %s was updated", params["id"])
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	stmt, err := db.Prepare("DELETE FROM user WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "User with ID = %s was deleted", params["id"])
+}
+
+func main() {
+	db, err = sql.Open("mysql", "root:0000@tcp(127.0.0.1:3306)/test")
+
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+	router.HandleFunc("/users/{id}", getUser).Methods("GET")
+	router.HandleFunc("/users/{id}", updateUser).Methods("PATCH")
+	router.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+
+	http.ListenAndServe(":8000", router)
+}
